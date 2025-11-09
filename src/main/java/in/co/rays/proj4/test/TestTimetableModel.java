32cc73df5e403e03e32abff38768ed4057fe9c26
Deepak9669 <deepakverma726800@gmail.com>
2025-11-09 08:28:27 +0530

    done

--- src/main/java/in/co/rays/proj4/test/TestTimetableModel.java
+++ src/main/java/in/co/rays/proj4/test/TestTimetableModel.java
@@ -70,7 +70,7 @@ public class TestTimetableModel {
 
 	}
 
-	public static void testUpdate() throws ParseException {
+	public static void testUpdate()  {
 
 		TimetableBean bean = new TimetableBean();
 		TimetableModel model = new TimetableModel();
@@ -93,7 +93,7 @@ public class TestTimetableModel {
 
 			model.update(bean);
 			System.out.println("TimeTable added sucessfully");
-		} catch (ApplicationException | DuplicateRecordException e) {
+		} catch (ApplicationException | DuplicateRecordException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}

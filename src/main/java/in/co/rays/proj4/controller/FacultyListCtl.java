fa52aad4258379c53fba6726ab3b53bcabfea53b
Deepak9669 <deepakverma726800@gmail.com>
2025-11-22 07:25:21 +0530

    done

--- src/main/java/in/co/rays/proj4/controller/FacultyListCtl.java
+++ src/main/java/in/co/rays/proj4/controller/FacultyListCtl.java
@@ -10,10 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import in.co.rays.proj4.bean.BaseBean;
 import in.co.rays.proj4.bean.FacultyBean;
+import in.co.rays.proj4.bean.SubjectBean;
 import in.co.rays.proj4.controller.BaseCtl;
 import in.co.rays.proj4.controller.ORSView;
 import in.co.rays.proj4.exception.ApplicationException;
 import in.co.rays.proj4.model.FacultyModel;
+import in.co.rays.proj4.model.SubjectModel;
 import in.co.rays.proj4.util.DataUtility;
 import in.co.rays.proj4.util.PropertyReader;
 import in.co.rays.proj4.util.ServletUtility;
@@ -67,15 +69,64 @@ public class FacultyListCtl extends BaseCtl {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		int pageNo = 1;
-		int pageSize = DataUtility.getInt(PropertyReader.getValue("page.size"));
+		List list = null;
+		List next = null;
+
+		int pageNo = DataUtility.getInt(request.getParameter("pageNo"));
+		int pageSize = DataUtility.getInt(request.getParameter("pageSize"));
+
+		pageNo = (pageNo == 0) ? 1 : pageNo;
+		pageSize = (pageSize == 0) ? DataUtility.getInt(PropertyReader.getValue("page.size")) : pageSize;
 
 		FacultyBean bean = (FacultyBean) populateBean(request);
 		FacultyModel model = new FacultyModel();
 
+		String op = DataUtility.getString(request.getParameter("operation"));
+		String[] ids = request.getParameterValues("ids");
+
 		try {
-			List<FacultyBean> list = model.search(bean, pageNo, pageSize);
-			List<FacultyBean> next = model.search(bean, pageNo + 1, pageSize);
+
+			if (OP_SEARCH.equalsIgnoreCase(op) || "next".equalsIgnoreCase(op) || "previous".equalsIgnoreCase(op)) {
+
+				if (OP_SEARCH.equalsIgnoreCase(op)) {
+					pageNo = 1;
+				} else if (OP_NEXT.equalsIgnoreCase(op)) {
+					pageNo++;
+				} else if (OP_PREVIOUS.equalsIgnoreCase(op)&& pageNo >1) {
+					pageNo--;
+				}
+
+			} else if (OP_NEW.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.FACULTY_CTL, request, response);
+				return;
+
+			} else if (OP_DELETE.equalsIgnoreCase(op)) {
+				pageNo = 1;
+				if (ids != null && ids.length > 0) {
+					FacultyBean deleteBean = new FacultyBean();
+					for (String id : ids) {
+
+						deleteBean.setId(DataUtility.getInt(id));
+						model.delete(deleteBean);
+
+						ServletUtility.setSuccessMessage("Faculty deleted successfully", request);
+
+					}
+
+				} else {
+					ServletUtility.setErrorMessage("Select at least one record", request);
+				}
+
+			} else if (OP_RESET.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.FACULTY_LIST_CTL, request, response);
+				return;
+
+			} else if (OP_BACK.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.FACULTY_LIST_CTL, request, response);
+				return;
+			}
+			list = model.search(bean, pageNo, pageSize);
+			next = model.search(bean, pageNo + 1, pageSize);
 
 			if (list == null || list.isEmpty()) {
 				ServletUtility.setErrorMessage("No record found", request);
--- src/main/java/in/co/rays/proj4/controller/MarksheetListCtl.java
+++ src/main/java/in/co/rays/proj4/controller/MarksheetListCtl.java
@@ -10,10 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import in.co.rays.proj4.bean.BaseBean;
 import in.co.rays.proj4.bean.MarksheetBean;
+import in.co.rays.proj4.bean.StudentBean;
 import in.co.rays.proj4.controller.BaseCtl;
 import in.co.rays.proj4.controller.ORSView;
 import in.co.rays.proj4.exception.ApplicationException;
 import in.co.rays.proj4.model.MarksheetModel;
+import in.co.rays.proj4.model.StudentModel;
 import in.co.rays.proj4.util.DataUtility;
 import in.co.rays.proj4.util.PropertyReader;
 import in.co.rays.proj4.util.ServletUtility;
@@ -68,15 +70,64 @@ public class MarksheetListCtl extends BaseCtl {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		int pageNo = 1;
-		int pageSize = DataUtility.getInt(PropertyReader.getValue("page.size"));
+		List list = null;
+		List next = null;
+
+		int pageNo = DataUtility.getInt(request.getParameter("pageNo"));
+		int pageSize = DataUtility.getInt(request.getParameter("pageSize"));
+
+		pageNo = (pageNo == 0) ? 1 : pageNo;
+		pageSize = (pageSize == 0) ? DataUtility.getInt(PropertyReader.getValue("page.size")) : pageSize;
 
 		MarksheetBean bean = (MarksheetBean) populateBean(request);
 		MarksheetModel model = new MarksheetModel();
 
+		String op = DataUtility.getString(request.getParameter("operation"));
+		String[] ids = request.getParameterValues("ids");
+
 		try {
-			List<MarksheetBean> list = model.search(bean, pageNo, pageSize);
-			List<MarksheetBean> next = model.search(bean, pageNo + 1, pageSize);
+
+			if (OP_SEARCH.equalsIgnoreCase(op) || "next".equalsIgnoreCase(op) || "previous".equalsIgnoreCase(op)) {
+
+				if (OP_SEARCH.equalsIgnoreCase(op)) {
+					pageNo = 1;
+				} else if (OP_NEXT.equalsIgnoreCase(op)) {
+					pageNo++;
+				} else if (OP_PREVIOUS.equalsIgnoreCase(op)&& pageNo >1) {
+					pageNo--;
+				}
+
+			} else if (OP_NEW.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.MARKSHEET_CTL, request, response);
+				return;
+
+			} else if (OP_DELETE.equalsIgnoreCase(op)) {
+				pageNo = 1;
+				if (ids != null && ids.length > 0) {
+					MarksheetBean deleteBean = new MarksheetBean();
+					for (String id : ids) {
+
+						deleteBean.setId(DataUtility.getInt(id));
+						model.delete(deleteBean);
+
+						ServletUtility.setSuccessMessage("MArksheet deleted successfully", request);
+
+					}
+
+				} else {
+					ServletUtility.setErrorMessage("Select at least one record", request);
+				}
+
+			} else if (OP_RESET.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.MARKSHEET_LIST_CTL, request, response);
+				return;
+
+			} else if (OP_BACK.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.MARKSHEET_LIST_CTL, request, response);
+				return;
+			}
+			list = model.search(bean, pageNo, pageSize);
+			next = model.search(bean, pageNo + 1, pageSize);
 
 			if (list == null || list.isEmpty()) {
 				ServletUtility.setErrorMessage("No record found", request);
